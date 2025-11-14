4a080f08987c4a70b4725f98aaed23b012bbb359
matteokeole <matteoo34@icloud.com>
2025-11-14 16:17:37 +0100

    feat: sequence<T> and view<T>

---
+++ Base/Types/base_sequence.hpp
@@ -0,0 +1,109 @@
+﻿// Copyright 2025 Atalante.
+// Licensed under MIT.
+
+#pragma once
+
+namespace ProjectA {
+	// TODO(upgrade): Allocate from arena.
+	template<typename T>
+	class base_sequence {
+		private:
+			static T* allocate(uint64 count) {
+				return new T[count]();
+			}
+
+			static void free(const T* data) {
+				delete[] data;
+			}
+
+		public:
+			base_sequence<T>() = default;
+
+			explicit base_sequence<T>(uint64 count) :
+				_count(count),
+				_data(allocate(_count))
+			{}
+
+			base_sequence<T>(std::initializer_list<T> list) :
+				base_sequence<T>(list.size())
+			{
+				copy(list.begin(), list.end(), _data);
+			}
+
+			base_sequence<T>(const base_sequence<T>& sequence) :
+				base_sequence<T>(sequence._count)
+			{
+				copy(sequence.begin(), sequence.end(), _data);
+			}
+
+			base_sequence<T>(base_sequence<T>&& sequence) noexcept :
+				_count(sequence._count)
+			{
+				_data = sequence._data;
+
+				sequence._count = 0;
+				sequence._data = nullptr;
+			}
+
+			~base_sequence<T>() {
+				free(_data);
+			}
+
+			void operator=(const base_sequence<T>& sequence) {
+				free(_data);
+
+				_count = sequence._count;
+				_data = allocate(_count);
+
+				copy(sequence.begin(), sequence.end(), _data);
+			}
+
+			void operator=(base_sequence<T>&& sequence) noexcept {
+				ASSERT(_data != sequence._data);
+
+				_count = sequence._count;
+				_data = sequence._data;
+
+				sequence._count = 0;
+				sequence._data = nullptr;
+			}
+
+			uint64 count() const {
+				return _count;
+			}
+
+			const T& operator[](uint64 index) const {
+				// TODO: Always allow 0?
+				ASSERT(index < _count);
+
+				return _data[index];
+			}
+
+			T& operator[](uint64 index) {
+				// TODO: Always allow 0?
+				ASSERT(index < _count);
+
+				return _data[index];
+			}
+
+			const T* begin() const {
+				return _data;
+			}
+
+			T* begin() {
+				return _data;
+			}
+
+			const T* end() const {
+				return _data + _count;
+			}
+
+			T* end() {
+				return _data + _count;
+			}
+
+		protected:
+			uint64 _count = 0;
+			T* _data = nullptr;
+	};
+}
---
+++ Base/Types/sequence.hpp
@@ -0,0 +1,46 @@
+﻿// Copyright 2025 Atalante.
+// Licensed under MIT.
+
+#pragma once
+
+#include "Types/base_sequence.hpp"
+#include "Types/base_view.hpp"
+
+namespace ProjectA {
+	// A container that encapsulates a dynamic size array.
+	// Unlike `std::vector<T>`, `sequence<T>` is not resizeable.
+	// It *should* be used when the number of elements is known at runtime, before creating the sequence.
+	// 
+	// NOTE: `T` **must** expose a default constructor.
+	template<typename T>
+	class sequence : public base_sequence<T> {
+		public:
+			using base_sequence<T>::base_sequence;
+	};
+
+	template<>
+	class sequence<char8> : public base_sequence<char8> {
+		public:
+			using base_sequence<char8>::base_sequence;
+
+			explicit sequence<char8>(const char8* data, uint64 count) :
+				sequence<char8>(count + 1)
+			{
+				_count -= 1;
+
+				copy(data, data + _count, _data);
+			}
+
+			sequence<char8>(const char8* data) :
+				sequence<char8>(data, std::strlen(data))
+			{}
+
+			sequence<char8>(base_view<char8> view) :
+				sequence<char8>(&view[0], view.count())
+			{}
+
+			sequence<char8>(const sequence<char8>& sequence) :
+				sequence(sequence.begin(), sequence.count())
+			{}
+	};
+}
