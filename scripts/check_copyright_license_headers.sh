b74e93d385c2015bee3dcb382616473523e8698d
matteokeole <matteoo34@icloud.com>
2025-11-14 16:20:54 +0100

    ci: Fix copyright license script

--- scripts/check_copyright_license_headers.sh
+++ scripts/check_copyright_license_headers.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 
-files=$(git diff --diff-filter=d --name-only origin/$GITHUB_BASE_REF origin/$GITHUB_HEAD_REF -- ***.{cpp,hpp,ipp})
+HEADER=$'// Copyright 2025 Atalante.\n// Licensed under MIT.'
+
+files=$(git diff --diff-filter=d --name-only origin/$GITHUB_BASE_REF origin/$GITHUB_HEAD_REF -- *.{cpp,hpp,ipp})
 unlicensed_files=()
 
 for file in $files; do
-	if [[ $(head -n 2 $file) != "$HEADER" ]]; then
+	line=$(head -n 2 $file | sed "s/^\xef\xbb\xbf//")
+
+	if [ "$line" != "$HEADER" ]; then
 		unlicensed_files+=($file)
 	fi
 done
 
 if [[ ${#unlicensed_files[@]} -gt 0 ]]; then
-	echo "The following file(s) don't start with a copyright license header." >> $GITHUB_STEP_SUMMARY
+	echo "The following file(s) don't have a valid copyright license header." >> $GITHUB_STEP_SUMMARY
 
 	for file in "${unlicensed_files[@]}"; do
 		echo "- [$file]($GITHUB_TREE_URL/$file)" >> $GITHUB_STEP_SUMMARY
