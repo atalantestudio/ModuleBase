3ded850832d2cb6519cde0a49f67ee3cab440fc5
matteokeole <matteoo34@icloud.com>
2025-11-29 00:27:48 +0100

    feat: sequence<char8> hash

--- Base/Hash.hpp
+++ Base/Hash.hpp
@@ -3,10 +3,27 @@
 
 #pragma once
 
+template<>
+struct std::hash<atl::sequence<atl::char8>> {
+	std::size_t operator()(const atl::sequence<atl::char8>& key) const {
+		std::size_t hash = 14695981039346656037;
+		atl::uint64 characterIndex = 0;
+
+		while (characterIndex < key.count()) {
+			hash ^= key[characterIndex];
+			hash *= 1099511628211;
+
+			characterIndex += 1;
+		}
+
+		return hash;
+	}
+};
+
 // TODO: It may be better to create a custom struct, e.g. ViewHasher.
 template<>
 struct std::hash<atl::view<atl::char8>> {
-	std::size_t operator()(const atl::view<atl::char8>& key) const {
+	std::size_t operator()(atl::view<atl::char8> key) const {
 		std::size_t hash = 14695981039346656037;
 		atl::uint64 characterIndex = 0;
 
