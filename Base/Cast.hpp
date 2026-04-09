8c19439d640f5a89d5b3ebdcc31d7ed92552f0fb
matteokeole <matteoo34@icloud.com>
2026-04-09 15:24:07 +0200

    feat: function_cast

--- Base/Cast.hpp
+++ Base/Cast.hpp
@@ -5,12 +5,17 @@
 
 namespace atl {
 	template<typename O, typename I>
-	typename std::enable_if<std::is_const<typename std::remove_pointer<O>::type>::value, O>::type pointer_cast(I pointer) {
+	inline O function_cast(I function) {
+		return reinterpret_cast<O>(function);
+	}
+
+	template<typename O, typename I>
+	inline typename std::enable_if<std::is_const<typename std::remove_pointer<O>::type>::value, O>::type pointer_cast(I pointer) {
 		return static_cast<O>(static_cast<const void*>(pointer));
 	}
 
 	template<typename O, typename I>
-	typename std::enable_if<!std::is_const<typename std::remove_pointer<O>::type>::value, O>::type pointer_cast(I pointer) {
+	inline typename std::enable_if<!std::is_const<typename std::remove_pointer<O>::type>::value, O>::type pointer_cast(I pointer) {
 		return static_cast<O>(static_cast<void*>(pointer));
 	}
 }
